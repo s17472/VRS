8d2f330a457fd3ad03a3f635fbad026688048e34
Mateusz Chodyna <s17606@pjwstk.edu.pl>
2021-01-22 23:45:19 +0100

    cleaning

--- src/Module.DIDN/detect.py
+++ src/Module.DIDN/detect.py
@@ -1,8 +1,5 @@
 import time
 import tensorflow as tf
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-if len(physical_devices) > 0:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 from absl import app
 from tensorflow.python.saved_model import tag_constants
 from PIL import Image
@@ -10,10 +7,12 @@ import cv2
 import numpy as np
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
-
-def main(_argv):
+def main():
     config = ConfigProto()
     config.gpu_options.allow_growth = True
     session = InteractiveSession(config=config)
@@ -47,7 +46,6 @@ def main(_argv):
         print(pred_bbox)
         for key, value in pred_bbox.items():
             pred_conf = value[:, :, 4:]
-        print(pred_conf)
         scores = tf.reshape(pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])).numpy()
 
         fps = 1.0 / (time.time() - start_time)
@@ -55,7 +53,6 @@ def main(_argv):
         # return value in % and fps
         if scores.size > 0: print(np.amax(scores))
         print("FPS: %.2f" % fps)
-
 
 
 if __name__ == '__main__':
